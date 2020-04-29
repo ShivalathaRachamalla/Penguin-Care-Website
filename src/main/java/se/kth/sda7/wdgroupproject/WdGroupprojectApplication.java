99c168be8ca25f2e32fa3eb3520d5c413e50f768
Manasa Muddasani <manasakanthala@gmail.com>
2020-04-29 13:39:31 +0200

    Initial Commit

---
+++ src/main/java/se/kth/sda7/wdgroupproject/WdGroupprojectApplication.java
@@ -0,0 +1,13 @@
+package se.kth.sda7.wdgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdGroupprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdGroupprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/kth/sda7/wdgroupproject/WdGroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.sda7.wdgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdGroupprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
