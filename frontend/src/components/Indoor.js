d8588971497cd686acaa30f700851f4f980fff41
Harshada Mulye <mulyeharshada@gmail.com>
2020-05-06 14:02:44 +0200

    Indoor file added

---
+++ frontend/src/components/Indoor.js
@@ -0,0 +1,147 @@
+import React, { Component } from "react";
+import '../App.css';
+class Indoor extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">Creative Activities to Keep Your Kids Entertained at Home :</h4>
+                    
+                   <div className="para">
+                   <img src={require('../img/activityani.gif')} alt="activity d
+                    image"  class="activity_img"/>
+                   <p>
+                   Whether your company has required that you work from home for a few weeks or your children's schools are closed, the 
+                   coronavirus outbreak means a lot more of time spent indoors. Of course, your kids' health and safety is paramount, but 
+                   keeping your little ones entertained for hours on end is probably vital for your sanity. In an effort to make the next few 
+                   weeks a little easier on parents, we came up with a list of easy, kid-friendly activities that will keep their minds occupied 
+                   at home, whether you're stocked up  on crafting supplies or not.
+                    </p>
+                    </div>
+                  
+                    <div className="para">
+                    
+                        <ol type="1">
+                        <font size="5" color="brown"> <li><b>Make homemade Playdough</b></li></font>
+                            
+                            <p>Any time we make homemade playdough, it’s a hit! 
+                                You don’t even need anything fancy like playdough tools. </p>
+                                <p>You’ll need:</p>
+                                <ul type="disc">
+                                    <li>flour</li>
+                                    <li>salt</li>
+                                    <li><a href="https://www.amazon.com/McCormick-Gourmet-Cream-Tartar-2-62/dp/B01KX8DBUO/ref=as_li_ss_tl?srs=7301146011&ie=UTF8&qid=1497115727&sr=8-2&keywords=cream+of+tartar&linkCode=sl1&tag=thissimplebal-20&linkId=2b321dc5a2985b62b7a9ef81f7d99336">cream of tartar </a>and</li>
+                                    <li><a href="https://www.amazon.com/McCormick-Color-Nature-0-51-oz/dp/B01LXUJ3J3/ref=as_li_ss_tl?srs=7301146011&ie=UTF8&qid=1497115791&sr=8-7&keywords=food+coloring&linkCode=sl1&tag=thissimplebal-20&linkId=1d75b755fbf60678105048d8d4985def">food coloring</a></li>
+                                
+                                </ul>
+                                <p className="para">Be careful not to overcook it, or the dough will be crumbly. You can use playdough tools, or simply let them play with the dough as is.</p>
+                                <img src={require('../img/playdough.jpg')} alt="activity d
+                             image" width="500" height="350" align="center" />
+                                
+                                <font size="5" color="brown">  <li><b>Make Fort</b></li></font> 
+                               
+                               <p>Building indoor forts is a timeless pastime that is still a popular activity for kids today. Here’s a list of the basic supplies you need to get started as well as our top tips for how to 
+                               create an extra special fort with your little one.</p>
+                               <p>Supplies:</p>
+                               <ul type="disc">
+                                   <li>Sheets and blankets</li>
+                                   <li>Pillows, blow-up mattresses and/or furniture cushions</li>
+                                   <li>Painter’s tape or command hooks</li>
+                                   <li>Stacks of heavy books</li>
+                               </ul>
+                               <img src={require('../img/fort.jpg')} alt="activity d
+                                image" width="600" height="350" align="center" />
+                              
+                              <font size="5" color="brown"> <li> <b>Jumping frog origami activity</b></li></font>
+                               <p> This is a great activity for school aged kids: 
+                                 <a href="https://www.youtube.com/watch?v=oi7oitREUBQ"> Origami jumping frogs </a>from It’s Always Autumn. Click through for step by step folding instructions, then let kids make as many frogs as they like. They actually jump, 
+                                   so they’re pretty fun to play with once you’ve folded a bunch.</p>
+                                   <img src={require('../img/jumpingfrog.gif')} alt="activity d
+                             image" width="500" height="250" align="center" />
+                                  
+                                  <font size="5" color="brown"> <li> <b>Rock Painting Activity</b></li></font>
+                                <p>With all the beautiful rock art floating around, we wanted to make sure that the littlest crafters don’t get left out.  Some of the story rocks and intricate paintings are too much for kids to craft.  
+                                 So we decided to search for some fun crafts that 
+                                 are perfect the smallest rock hunters.  Grab some paints, a handful of rocks, and your little ones and let’s start stone painting!</p>
+                                 <img src={require('../img/rock_collection.jpg')} alt="activity d
+                                image" width="600" height="350" align="center" />
+                                 
+                                 <font size="5" color="brown"> <li> <b>Fingerprint Art</b></li></font>
+                             <p>My little kids have a new favorite activity: fingerprint art. They have spent hours in the past few weeks making finger and thumb prints and 
+                                 then adding details with a black Sharpie to turn the prints into all sorts of animals, monsters, bugs, etc. It’s a fantastic boredom buster: it keeps them busy and quiet without
+                                  the aid of electronics or any help from me.
+                                  And as long as we have wipes handy for them to clean up with, it doesn’t even make a mess.</p>
+                                  <img src={require('../img/finger_art.jpg')} alt="activity d
+                             image" width="600" height="350" align="center" />
+                            <font size="5" color="brown"> <li> <b>Create a Race-Car Track</b></li></font>
+                             <p>Use your painter’s tape (or opt for this super cool race-track tape) to make a huge road system for your kids’
+                                  matchbox cars. Think outside the box and have the track scale furniture and other obstacles (just make sure it’s something 
+                                  you don’t mind toy vehicles being “driven” over). The key to making it active is to make it large-scale, so they are engaging their core muscles while crawling all over.</p>
+                                  <img src={require('../img/car_tape.jpg')} alt="activity d
+                             image" width="550" height="350" align="center" />
+                             <font size="5" color="brown"> <li> <b> Paint with a Balloon</b></li></font>
+                             <p>Make cricket-ball sized water balloons and use them as tools to paint on blank paper. Young kids can simply make stamps in different colours. Older kids could use 
+                                 these to paint simple structured images.</p>
+                                 <img src={require('../img/balloonart.jpg')} alt="activity d
+                             image" width="400" height="250" align="center" />
+                             <font size="5" color="brown"> <li> <b> Paper Plate Sun Craft</b></li></font>
+                             <p>
+                             Here comes the sun! This fun and simple paper plate craft will definitely work on those 
+                             fine motor skills, as little ones carefully glue each of the sun’s “rays” on (AKA yellow pipe cleaners). You can 
+                             also practice counting with your tot as she crafts to work on those math skills.  </p>
+                             <p>What you will nead:</p>
+                             <ul type="disc">
+                             <li> 1 small yellow paper plate (or a white one your toddler can paint yellow) </li>
+                            <li>1 single hole punch</li>
+                            <li>Yellow straws</li>
+                            <li>Scissors</li>
+                           <li>Yellow tissue paper squares</li>
+                           <li>Liquid glue</li>
+                            </ul>
+                            <img src={require('../img/sun.jpg')} alt="activity d
+                             image" width="500" height="300" align="center" />
+                            <font size="5" color="brown"> <li> <b>Caterpillar Suncatcher Craft</b></li></font>
+                            <p>This caterpillar suncatcher offers toddlers a fun indoor activity. Using the colorful tops of extra Play-Doh lids, you can help guide 
+                             your little one as he sticks them to the window, forming a happy little caterpillar. Then show him where to glue on the 
+                             tiny caterpillar legs, ears and eyes to complete the friendly face. </p>
+                             <p>What you will nead:</p>
+                             <ul type="disc">
+                                 <li>Play-Doh lids (5 small and 1 large)</li>
+                                 <li>Glue dots</li>
+                                 <li>2 small wiggly eyes</li>
+                                 <li>Black permanent marker</li>
+                                 <li>Tape</li>
+                                 <li>Scissors</li>
+                             </ul>
+                             <img src={require('../img/Caterpillar.jpg')} alt="activity d
+                             image" width="500" height="300" align="center" />
+                              <font size="5" color="brown"> <li> <b>Puzzles</b></li></font>
+                              <p>Exercise those creative, cognitive and problem-solving muscles with a good puzzle. You can use a 
+                                  store-bought variety or have the kids make their own. Have your children draw a picture on a sturdy 
+                                  piece of cardboard or Bristol board. Then use a pencil to outline puzzle pieces directly on their drawing. 
+                                  Cut out the pieces with a good pair of scissors, mix them up and get solving. Indoor games and craft in one fun activity!</p>
+                                  <img src={require('../img/puzzle.gif')} alt="activity d
+                             image" width="500" height="300" align="center" />
+                            </ol>
+                            <br/>
+                            <img src={require('../img/indactivity.png')} alt="activity d
+                             image" width="500" height="500" align="center" /><br/>
+                             <font color="red"><h2 align="center">STAY AT HOME</h2></font>
+                             <p className="para">Above all, we hope that staying at home will save lives. Indeed, we hope, that staying at home, 
+                                 will save the enormous work and limitless efforts of all the people around the world who fight daily 
+                                 to save lives. And of course, we hope, that staying at home, will help us to have our lives back. Staying at 
+                                   home makes us appreciate all the freedom we have and nature we do not always protect.
+
+                                  Stay sane, happy and healthy!
+
+                                  Happy Playing!</p>
+                    </div>
+                     
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Indoor;
