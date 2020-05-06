53ad21384e97418c5314ecfc1ee4bd5154fe0f9e
Harshada Mulye <mulyeharshada@gmail.com>
2020-05-06 15:03:36 +0200

    navigation of indoor game change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,11 +14,14 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
           <Link className="navbar-brand" to={"/"}><img className="logo" src={require("./assets/images/finallogo.png")} alt="logo"/></Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
+          <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
                 <Link className="nav-link" to={"/Indoor"}>Indoor Activity</Link>
               </li>
+              </ul>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+            
               <li className="nav-item">
                 <Link className="nav-link" to={"/Login"}>Login</Link>
               </li>
