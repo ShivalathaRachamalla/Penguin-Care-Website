901eb40b9e08d9c92bfaeb3bd6f7e2089c3d6682
Anu Sindhura Madhavapeddy <anusindhuram@gmail.com>
2020-05-17 12:05:21 +0200

    added backend code for post img

--- src/main/java/se/kth/sda7/wdgroupproject/posts/PostController.java
+++ src/main/java/se/kth/sda7/wdgroupproject/posts/PostController.java
@@ -2,7 +2,10 @@ package se.kth.sda7.wdgroupproject.posts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import se.kth.sda7.wdgroupproject.recepies.Recepie;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,6 +18,14 @@ public class PostController {
     @PostMapping("")
     public Post create(@RequestBody Post newPost) {
         return postService.save(newPost);
+    }
+    @PostMapping("")
+    public Post create(@RequestParam("file") MultipartFile file, @RequestParam("body") String body,@RequestParam("email") String email)throws IOException {
+        Post  post = new Post();
+        post.setBody(body);
+        post.setImg(file.getBytes());
+        post.setEmail(email);
+        return postService.save(post);
     }
 
 
